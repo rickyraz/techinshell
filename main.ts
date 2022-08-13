7b7f651d0a62e625493683f9792c2c79f4fbbbc6
rickyraz <rickyraihan83@gmail.com>
2022-08-13 08:29:18 +0700

    add-google-analytic

--- main.ts
+++ main.ts
@@ -16,17 +16,16 @@ blog({
   theme: "light",
   favicon : "./rick.jpg",
   lang: "en",
-  // middlewares: [
+  middlewares: [
 
-  //   // If you want to set up Google Analytics, paste your GA key here.
-  //   // ga("UA-XXXXXXXX-X"),
+    // If you want to set up Google Analytics, paste your GA key here.
+    ga("G-39L44RHQ2Y"),
 
-  //   // If you want to provide some redirections, you can specify them here,
-  //   // pathname specified in a key will redirect to pathname in the value.
-  //   // redirects({
-  //   //  "/hello_world.html": "/hello_world",
-  //   //  ""
-  //   // }),
+    // If you want to provide some redirections, you can specify them here,
+    // pathname specified in a key will redirect to pathname in the value.
+    redirects({
+     "/hello_world.html": "/hello_world",
+    }),
 
-  // ]
+  ]
 });
