d62f6ef92b647e74d1701eb69266c598eadf0696
rickyraz <rickyraihan83@gmail.com>
2022-08-13 08:21:00 +0700

    first-commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+/** @jsx h */
+import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.2/blog.tsx";
+
+blog({
+  title: "Ricky Azhari",
+  description: `self-taught software engineer & rustaceans`,
+  avatar: "./rick.jpg",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "mailto:rickyrazhari83@gmail.com" },
+    { title: "GitHub", url: "https://github.com/rickyraz" },
+    { title: "Twitter", url: "https://twitter.com/rickyrazhari8" },
+    { title: "Project", url: "https://rick-realestate.vercel.app/", target:"_blank" },
+  ],
+  author: "Ricky Azhari",
+  theme: "light",
+  favicon : "./rick.jpg",
+  lang: "en",
+  // middlewares: [
+
+  //   // If you want to set up Google Analytics, paste your GA key here.
+  //   // ga("UA-XXXXXXXX-X"),
+
+  //   // If you want to provide some redirections, you can specify them here,
+  //   // pathname specified in a key will redirect to pathname in the value.
+  //   // redirects({
+  //   //  "/hello_world.html": "/hello_world",
+  //   //  ""
+  //   // }),
+
+  // ]
+});
