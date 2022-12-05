aedadc82b91f2c9190e5c351340411d1e578e66b
rickyrazhari <rickyraihan83@gmail.com>
2022-12-05 21:56:23 +0700

    update_link

--- main.ts
+++ main.ts
@@ -10,22 +10,24 @@ blog({
     { title: "Email", url: "mailto:rickyrazhari83@gmail.com" },
     { title: "GitHub", url: "https://github.com/rickyraz" },
     { title: "Twitter", url: "https://twitter.com/rickyrazhari8" },
-    { title: "Project", url: "https://rick-realestate.vercel.app/", target:"_blank" },
+    {
+      title: "Portfolio",
+      url: "https://rickraz-portfolio.vercel.app/",
+      target: "_blank",
+    },
   ],
   author: "Ricky Azhari",
   theme: "light",
-  favicon : "./rick.jpg",
+  favicon: "./rick.jpg",
   lang: "en",
   middlewares: [
-
     // If you want to set up Google Analytics, paste your GA key here.
     ga("G-39L44RHQ2Y"),
 
     // If you want to provide some redirections, you can specify them here,
     // pathname specified in a key will redirect to pathname in the value.
     redirects({
-     "/hello_world.html": "/hello_world",
+      "/hello_world.html": "/hello_world",
     }),
-
-  ]
+  ],
 });
